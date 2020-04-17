be12d067bc63a60b8e3b92fa29ec2734180403c4
Yongjae <k33721@gmail.com>
2020-04-17 18:54:51 +0900

    (3)-2

--- userdao/src/main/java/kr/ac/jejunu/user/UserDao.java
+++ userdao/src/main/java/kr/ac/jejunu/user/UserDao.java
@@ -2,7 +2,7 @@ package kr.ac.jejunu.user;
 
 import java.sql.*;
 
-public class UserDao {
+public abstract class UserDao {
     public User get(Integer id) throws ClassNotFoundException, SQLException {
         //mysql
         //driver 로딩
@@ -45,9 +45,8 @@ public class UserDao {
         connection.close();
     }
 
-    private Connection getConnection() throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        //connection
-        return DriverManager.getConnection("jdbc:mysql://localhost/jeju?serverTimezone=Asia/Seoul", "yongjae", "1234");
-    }
+    abstract public Connection getConnection() throws ClassNotFoundException, SQLException ;
+//        Class.forName("com.mysql.cj.jdbc.Driver");
+//        //connection
+//        return DriverManager.getConnection("jdbc:mysql://localhost/jeju?serverTimezone=Asia/Seoul", "yongjae", "1234");
 }
