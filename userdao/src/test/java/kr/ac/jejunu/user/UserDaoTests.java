77bf6ca12d0ac46b77529bf9018306729a02f58d
Yongjae <k33721@gmail.com>
2020-06-20 00:24:00 +0900

    (12)

--- userdao/src/test/java/kr/ac/jejunu/user/UserDaoTests.java
+++ userdao/src/test/java/kr/ac/jejunu/user/UserDaoTests.java
@@ -30,60 +30,60 @@ public class UserDaoTests {
         userDao = applicationContext.getBean("userDao", UserDao.class);
     }
 
-    @Test
-    public void get() throws SQLException, ClassNotFoundException {
-        Integer id = 1;
-
-        DaoFactory daoFactory = new DaoFactory();
-
-        User user = userDao.get(id);
-        assertThat(user.getId(), is(id));
-        assertThat(user.getName(), is(name));
-        assertThat(user.getPassword(), is(password));
-    }
-
-    @Test
-    public void insert() throws SQLException, ClassNotFoundException {
-        User user = new User();
-        user.setName(name);
-        user.setPassword(password);
-        userDao.insert(user);
-        assertThat(user.getId(), greaterThan(0));
-
-        User insertedUser = userDao.get(user.getId());
-        assertThat(insertedUser.getName(), is(name));
-        assertThat(insertedUser.getPassword(), is(password));
-    }
-
-    @Test
-    public void update() throws SQLException {
-        User user = new User();
-        user.setName(name);
-        user.setPassword(password);
-        userDao.insert(user);
-
-        String updatedName = "강용재";
-        String updatedPassword = "1111";
-        user.setName(updatedName);
-        user.setPassword(updatedPassword);
-
-        userDao.update(user);
-
-        User updatedUser = userDao.get(user.getId());
-        assertThat(updatedUser.getName(), is(updatedName));
-        assertThat(updatedUser.getPassword(), is(updatedPassword));
-    }
-
-    @Test
-    public void delete() throws SQLException {
-        User user = new User();
-        user.setName(name);
-        user.setPassword(password);
-        userDao.insert(user);
-
-        userDao.delete(user.getId());
-
-        User deletedUser = userDao.get(user.getId());
-        assertThat(deletedUser, IsNull.nullValue());
-    }
+//    @Test
+//    public void get() throws SQLException, ClassNotFoundException {
+//        Integer id = 1;
+//
+//        DaoFactory daoFactory = new DaoFactory();
+//
+//        User user = userDao.get(id);
+//        assertThat(user.getId(), is(id));
+//        assertThat(user.getName(), is(name));
+//        assertThat(user.getPassword(), is(password));
+//    }
+//
+//    @Test
+//    public void insert() throws SQLException, ClassNotFoundException {
+//        User user = new User();
+//        user.setName(name);
+//        user.setPassword(password);
+//        userDao.insert(user);
+//        assertThat(user.getId(), greaterThan(0));
+//
+//        User insertedUser = userDao.get(user.getId());
+//        assertThat(insertedUser.getName(), is(name));
+//        assertThat(insertedUser.getPassword(), is(password));
+//    }
+//
+//    @Test
+//    public void update() throws SQLException {
+//        User user = new User();
+//        user.setName(name);
+//        user.setPassword(password);
+//        userDao.insert(user);
+//
+//        String updatedName = "강용재";
+//        String updatedPassword = "1111";
+//        user.setName(updatedName);
+//        user.setPassword(updatedPassword);
+//
+//        userDao.update(user);
+//
+//        User updatedUser = userDao.get(user.getId());
+//        assertThat(updatedUser.getName(), is(updatedName));
+//        assertThat(updatedUser.getPassword(), is(updatedPassword));
+//    }
+//
+//    @Test
+//    public void delete() throws SQLException {
+//        User user = new User();
+//        user.setName(name);
+//        user.setPassword(password);
+//        userDao.insert(user);
+//
+//        userDao.delete(user.getId());
+//
+//        User deletedUser = userDao.get(user.getId());
+//        assertThat(deletedUser, IsNull.nullValue());
+//    }
 }
